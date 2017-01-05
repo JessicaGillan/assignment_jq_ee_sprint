ddb80445a313dceee511faa20c2bf389970d3d04
Jessica G <gillan.jessica@gmail.com>
2017-01-05 14:57:55 -0700

    finish dropdown

--- dropdown-script.js
+++ dropdown-script.js
@@ -5,7 +5,8 @@ var $box = $(".display-box")
 $box.click(function(){
   $("ul").slideDown(1000);
 });
+
 $("ul").on("click", "li", function(e) {
-  $(".text-box").text(e.target.innerText);
-  $("ul").slideUp(0);
+  $(".display-box").val(e.target.innerText);
+  $("ul").slideUp(500);
 });
