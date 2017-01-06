f5ba5e10f108643aa0838a00e038b24db9ddd50c
Jessica G <gillan.jessica@gmail.com>
2017-01-05 17:02:04 -0700

    add boxes on click

--- photo-tagging.js
+++ photo-tagging.js
@@ -1,5 +1,9 @@
-var $photo = $( ".photo" )
-var $box = $(".box")
+var $photo = $( ".photo" ),
+    $box = $(".box"),
+    $dropdown = $('<ul>').html(
+      '<li class="choice">Bob</li><li class="choice">Frank</li><li class="choice">Waldo</li>'
+    ).addClass('push-down').slideUp(0);
+
 $photo.mousemove(function( event ) {
   $box.css("left", (event.pageX - 85) + "px");
   $box.css("top", (event.pageY - 40) + "px");
@@ -7,4 +11,13 @@ $photo.mousemove(function( event ) {
 
 $photo.hover(function( event ) {
   $box.toggleClass("finder");
+});
+
+$photo.click(function( event ) {
+  newDiv = $('<div>').css("left", (event.pageX - 85) + "px")
+                     .css("top", (event.pageY - 40) + "px")
+                     .addClass("finder box");
+  newDiv.append($dropdown).slideDown(500);
+
+  $photo.append(newDiv);
 });
